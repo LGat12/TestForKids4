0bd719c4cd555d8276d901eb97a26886b8f5c5d8
LGat12 <128309068+LGat12@users.noreply.github.com>
2024-01-30 17:07:19 +0200

    Add files via upload

---
+++ TestForKids/View/AddUser.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using TestForKids.Model;
+using TestForKids.ViewModel;
+
+namespace TestForKids.View
+{
+    /// <summary>
+    /// Interaction logic for AddUser.xaml
+    /// </summary>
+    public partial class AddUser : Window
+    {
+        private SharedViewModel _sharedViewModel;
+
+        public AddUser(SharedViewModel sharedViewModel)
+        {
+            InitializeComponent();
+            _sharedViewModel = sharedViewModel;
+
+        }
+
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Get data from textboxes
+                int userId = int.Parse(txtUserId.Text);
+                string firstName = txtFname.Text;
+                string lastName = txtLname.Text;
+                string city = txtCity.Text;
+                string email = txtEmail.Text;
+
+                // Create a new user
+                User newUser = new User
+                {
+                    UserId = userId,
+                    FirstName = firstName,
+                    LastName = lastName,
+                    City = city,
+                    State = "",
+                    Country = "",
+                    Email = email,
+                    Password = "",
+                    /*
+                  
+                        private int userId;
+                        private string firstName;
+                        private string lastName;
+                        private string city;
+                        private string state;
+                        private string country;
+                        private string eMail;
+                        private string password;*/
+                };
+
+                _sharedViewModel.UsersList.Add(newUser);
+                MessageBox.Show("User added successfully!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+
+
+                // Optionally, clear the textboxes after adding the user
+                ClearTextBoxes();
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+        private void ClearTextBoxes()
+        {
+            // Clear the content of all textboxes
+            txtUserId.Clear();
+            txtFname.Clear();
+            txtLname.Clear();
+            txtCity.Clear();
+            txtEmail.Clear();
+        }
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                LoginPage loginPage = new LoginPage(_sharedViewModel);
+
+                // Show the LoginPage
+                loginPage.Show();
+
+                // Close the current window
+                this.Close();
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Error hh", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+       
+
+
+
+    }
+}
---
+++ TestForKids/View/Login.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TestForKids.Model;
+using TestForKids.ViewModel;
+
+namespace TestForKids.View
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class LoginPage : Window
+    {
+        private SharedViewModel _sharedViewModel;
+       // private UserViewModel _userViewModel;
+
+        public LoginPage(SharedViewModel sharedViewModel)
+        {
+            InitializeComponent();
+            _sharedViewModel = sharedViewModel;
+        }
+
+        private void UsrTxtBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            // Clear the text when the TextBox gets focus
+            UsrTxtBox.Text = "";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Get the entered email and password 
+                string enteredEmail = UsrTxtBox.Text;
+                string enteredPassword = PwdBox.Password;
+
+                // Search for a match in the list
+                User matchedUser = null;
+                 
+                foreach (User user in _sharedViewModel.UsersList)
+                {
+                    if (user.Email == enteredEmail && user.Password == enteredPassword)
+                    {
+                        matchedUser = user;
+                        break; // Found a match, exit the loop
+                    }
+                }
+
+
+                // Check if a match was found and load the main page
+                if (matchedUser != null)
+                {
+                    // show the main page
+                    EnterGame game = new EnterGame(_sharedViewModel, matchedUser.FirstName, matchedUser.LastName);
+                    MainPage mainPage = new MainPage(_sharedViewModel);
+                    game.Show();
+
+                    // Close the login page
+                    this.Close();
+                }
+                else
+                {
+                    // No match found, display an error message
+                    MessageBox.Show("Invalid email or password. Please try again.");
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions that may occur
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+    }
+}
